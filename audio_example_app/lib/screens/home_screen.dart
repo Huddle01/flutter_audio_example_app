a8f47c9554106c2baf718bf2b7aebae2db9040ba
ambjn <jainanuamber@gmail.com>
2023-11-06 15:49:35 +0530

    added platform specific permissions

---
+++ audio_example_app/lib/screens/home_screen.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:huddle01_flutter_client/huddle01_flutter_client.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String projectId = 'XBQsbfXdUWW7YlkyF9Yb7BSft6aILeOW';
+  String roomId = "zth-aegg-lgb";
+
+  getPermissions() async {
+    await Permission.camera.request();
+    await Permission.microphone.request();
+  }
+
+  @override
+  void initState() {
+    getPermissions();
+    huddleClient.huddleEventListeners();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  HuddleClient huddleClient = HuddleClient();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "Huddle01\nFlutter Audio App 🎵",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          )),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              Expanded(
+                  child: Column(
+                children: [
+                  TextButton.icon(
+                      onPressed: () {
+                        huddleClient.initialize(projectId);
+                      },
+                      icon: const Icon(Icons.start_sharp),
+                      label: const Text("INITIALIZE")),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  TextButton.icon(
+                      onPressed: () {
+                        huddleClient.joinLobby(roomId);
+                      },
+                      icon: const Icon(Icons.room_service),
+                      label: const Text("JOIN LOBBY")),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  TextButton.icon(
+                      onPressed: () async {
+                        await huddleClient.joinRoom();
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.room_preferences),
+                      label: const Text("JOIN ROOM")),
+                ],
+              )),
+              Expanded(
+                child: Column(
+                  children: [
+                    TextButton.icon(
+                        onPressed: () async {
+                          await huddleClient.fetchAudioStream();
+                        },
+                        icon: const Icon(Icons.audiotrack),
+                        label: const Text("FETCH AUDIO")),
+                    TextButton.icon(
+                        onPressed: () async {
+                          await huddleClient
+                              .produceAudio(huddleClient.getAudioStream());
+                          setState(() {});
+                        },
+                        icon: const Icon(Icons.mic),
+                        label: const Text("START MIC")),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    TextButton.icon(
+                        onPressed: () {
+                          huddleClient.stopProducingAudio();
+                          setState(() {});
+                        },
+                        icon: const Icon(Icons.mic_off),
+                        label: const Text("STOP MIC")),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          TextButton.icon(
+              onPressed: () async {
+                await huddleClient.leaveRoom();
+              },
+              icon: const Icon(Icons.door_back_door),
+              label: const Text("LEAVE ROOM")),
+          const SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ValueListenableBuilder(
+              valueListenable: peersList,
+              builder: (ctx, val, _) {
+                return Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blueGrey.shade100),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "peers in the room 🧑‍🤝‍🧑",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        val.isNotEmpty ? "${val.keys}" : '',
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 15, color: Colors.blueGrey),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
