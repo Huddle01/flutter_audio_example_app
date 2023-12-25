8c696c814717e53788b1733d3c899fbd0668d373
Amber Jain <jainanuamber@gmail.com>
2023-12-25 06:51:52 +0530

    updated colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class SpacesColors {
   static const Color mainBgColor = Color.fromARGB(255, 47, 47, 47);
-  static const Color micColor = Color.fromARGB(255, 215, 43, 109);
+  static const Color micOffColor = Color.fromARGB(255, 215, 43, 109);
+  static const Color micOnColor = Color.fromARGB(255, 24, 180, 13);
   static const Color mainColor = Color(0xFF7366D7);
 }
